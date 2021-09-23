cb838387d3c04435b8a40a927d1e3aeee82ac141
jhowsemir Fernandes <josemir.famorim@gmail.com>
2021-09-23 18:03:29 -0300

    certo

--- react/site/src/service/api.js
+++ react/site/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://schooldevjo.herokuapp.com/'
+    baseURL: 'https://schooldevjo.herokuapp.com'
 })
 
 export default class Api {
